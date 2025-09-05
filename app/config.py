ca89107460be835444427349cb6eed09066b53a5
AndresCastilloPlata <plataandres26@gmail.com>
2025-09-04 19:07:11 -0500

    ajuste del config.py y requirement.txt

--- app/config.py
+++ app/config.py
@@ -1,15 +1,18 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# Archivo: app/config.py
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # Aquí defines todas las variables que esperas leer del archivo .env
     DATABASE_URL: str
     REDIS_URL: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    # Esta línea le dice a Pydantic que lea las variables de entorno en produccion
-    model_config = SettingsConfigDict()
+    # Usamos 'Config' en lugar de 'model_config' para mayor compatibilidad
+    class Config:
+        # Esta línea es la clave, busca el archivo si existe
+        env_file = ".env"
+        # Esta línea indica que si hay un error, no falle silenciosamente
+        env_file_encoding = 'utf-8'
 
-# Creamos una única instancia que usaremos en toda la aplicación
 settings = Settings()
